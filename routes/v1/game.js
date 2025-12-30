39d33588f88450964870f91ee385f5d741e836d1
Valentin1302 <valentingautier8@gmail.com>
2025-12-30 15:55:37 +0100

    amélioration architecture mise en place middleware

--- routes/v1/game.js
+++ routes/v1/game.js
@@ -1,7 +1,13 @@
-const { Router } = require("express");
-const gameController = require("../../controllers/game");
+const { Router } = require('express');
+const gameController = require('../../controllers/game');
+const versioning = require('../../middlewares/versioning'); 
+const i18n = require('../../middlewares/i18');
+const { validateGame } = require('../../middlewares/game');
 
 const router = new Router();
+
+router.use(versioning);
+router.use(i18n);
 
 router.get("/games", gameController.cget);
 router.post("/games", gameController.post);
